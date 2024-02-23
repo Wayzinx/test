a08b3cb20f8af7fb599442f9e8ff05b3c590c3af
wayzinx <wayzinx@outlook.com>
2024-02-23 22:41:09 +0800

    提交

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/test")
+@ResponseBody
+public class TestController {
+
+    @GetMapping("/test")
+    public String createOrder()  {
+         return "hello world";
+    }
+}
